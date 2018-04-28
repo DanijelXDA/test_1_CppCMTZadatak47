9dba4c40ea0e84de76cab1f4c9eac8c0f5bcb5b7
Danijel Jovanovic <36568690+DanijelXDA@users.noreply.github.com>
2018-04-28 20:19:04 +0200

    Korekcija rešenja zadatka od strane predavača

--- zadatak47.c
+++ zadatak47.c
@@ -1,89 +1,132 @@
 #include <iostream>
 using namespace std;
 
-class Skup {
+class Skup{
+
     private:
-        int *pok;
+      int *pok;
     public:
-        Skup() {
+        Skup(){
             pok = new int[10];
 
-            for(int i = 0; i < 10; ++i) {
+            for(int i = 0; i < 10; ++i){
                 pok[i] = 0;
             }
         }
 
-        Skup(int *niz, int duzina) {
+        Skup(int *p, int n){
+
             pok = new int[10];
 
-            for(int i = 0; i < 10; ++i) {
-                if( i < duzina && duzina < 10) {
-                    pok[i] = niz[i];
-                }
-                else {
+            for(int i = 0; i < 10; ++i){
+                if(i < n){
+                    pok[i] = p[i];
+                }else{
                     pok[i] = 0;
+
                 }
+
             }
+
         }
 
-        Skup(const Skup &sk) {
+        Skup(const Skup &s){
+            pok = new int[10];
 
             for(int i = 0; i < 10; ++i){
-                pok[i] = sk.pok[i];
+                if(i < 10){
+                    pok[i] = s.pok[i];
+                }else{
+                    pok[i] = 0;
+
+                }
+
             }
 
         }
-
         Skup& operator=(const Skup &s) {
-            for(int i = 0; i < 10; ++i){
-                if( s.pok[i] % 2 == 0) {
-                    s.pok[i] = 0;
-                }
-                else {
-                    s.pok[i] = 1;
-                }
+          for(int i = 0; i < 10; ++i) {
+            if(s.pok[i] % 2 == 0) {
+              pok[i] = 0;
             }
-
+            else {
+              pok[i] = 1;
+            }
+          }
+        return *this;
         }
 
         Skup& operator-=(const Skup &s) {
-            for(int i = 0; i < 10; ++i)
-                for(int j = 0; j < 10; ++j) {
-                if( pok[i] == s.pok[j]) {
-                    pok[i] = 1;
-                }
-                else {
-                    pok[i] = 0;
-                }
+          for(int i = 0; i < 10; ++i) {
+            int temp = pok[i];
+            pok[i] = 0;
+
+            for(int j = 0; j < 10; ++j) {
+              if(temp == s.pok[j]) {
+                pok[i] = 1;
+                break;
+              }
             }
+          }
+          return *this;
         }
 
-        friend Skup& operator==(const Skup &s1, const Skup &s2) {
-            bool uspesno = false;
+        friend bool operator==(const Skup &s1, const Skup &s2) {
+          bool uspesno = true;
 
-            for(int i = 0; i < 10; ++i)
-                for(int j = 0; j < 10; ++j) {
-                    if(s1.pok[i] == s2.pok[j]) {
-                        uspesno = true;
-                    }
+          if(&s1 != &s2) {
+            for(int i = 0; i < 9; ++i) {
+              bool pronadjen = false;
+              for(int j = 0; j < 10; ++j) {
+                if(s1.pok[i] == s2.pok[j]) {
+                  pronadjen = true;
+                  break;
+                }
+              }
+              if(!pronadjen) {
+                uspesno = false;
+                break;
+              }
             }
-            if(uspesno)
-                cout << "Nizovi su jednaki" << endl;
+          }
+
+          return uspesno;
         }
 
         friend ostream& operator<<(ostream &out, const Skup &s) {
-            for(int i = 0; i < 10; ++i) {
-                out << "{ " << s.pok[i] << ", ";
-                out << " }";
+          out << "{";
+
+          for(int i = 0; i < 10; ++i) {
+            out << s.pok[i];
+
+            if(i < 9) {
+              out << ", ";
             }
-            return out;
+
+          }
+          out << "}";
+
+          return out;
         }
+
 };
 
-int main(void) {
+int main(){
 
-    int niz[10] = {5, 31, 8, 4, 6, 9, 12, 3, 7, 14};
-    Skup s1(niz, 10);
+    Skup s1;
 
+    int niz1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    int niz2[] = {8, -11, 9, 20, 3};
+
+    Skup s2(niz1, 15);
+    Skup s3(niz2, 5);
+
+    cout << (s2 == s2 ? "DA" : "NE") << endl;
+    cout << (s2 == s3 ? "DA" : "NE") << endl;
+
+    cout << "s1: " << s1 << endl;
+    cout << "s2: " << s2 << endl;
+    cout << "s3: " << s3 << endl;
     return 0;
+
 }
